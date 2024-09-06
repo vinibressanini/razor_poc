08b7411c90d5fc044636b532e19b508543bb2856
vinibressanini <vinimarcus41@gmail.com>
2024-09-06 17:26:10 -0300

    env: created and modified dbcontext

---
+++ RazorApp/Data/AppDbContext.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RazorApp.Models;
+
+namespace RazorApp.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<Person>(entity =>
+            {
+                entity.Property(e => e.Id).HasColumnName("Id").IsRequired();
+                entity.Property(e => e.Name).HasColumnName("Name").IsRequired();
+                entity.Property(e => e.Email).HasColumnName("Email").IsRequired();
+                entity.HasMany(t => t.Courses).WithMany(t => t.Students)
+                .UsingEntity("PersonCourse",
+                l => l.HasOne(typeof(Person)).WithMany().HasForeignKey("PersonId").HasPrincipalKey("Id"),
+                r => r.HasOne(typeof(Course)).WithMany().HasForeignKey("CourseId").HasPrincipalKey("Id"),
+                j => j.HasKey("PersonId","CourseId"));
+            });
+
+            modelBuilder.Entity<Course>(entity =>
+            {
+                entity.Property(e => e.Id).HasColumnName("Id").IsRequired();
+                entity.Property(e => e.Name).HasColumnName("Name").IsRequired();
+                entity.HasMany(t => t.Students).WithMany(t => t.Courses)
+                .UsingEntity("PersonCourse",
+                l => l.HasOne(typeof(Course)).WithMany().HasForeignKey("CourseId").HasPrincipalKey("Id"),
+                r => r.HasOne(typeof(Person)).WithMany().HasForeignKey("PersonId").HasPrincipalKey("Id"),
+                j => j.HasKey("PersonId", "CourseId"));
+            });
+
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Person> Person { get; set; } = default!;
+        public DbSet<Course> Course { get; set; } = default!;
+    }
+}
---
+++ RazorApp/Pages/People/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RazorApp.Data;
+using RazorApp.Models;
+
+namespace RazorApp.Pages.People
+{
+    public class IndexModel : PageModel
+    {
+        private readonly RazorApp.Data.AppDbContext _context;
+
+        public IndexModel(RazorApp.Data.AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Person> Person { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            Person = await _context.Person.ToListAsync();
+        }
+    }
+}
